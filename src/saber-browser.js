4052198ea12c901dc5bf91ff721c7fdfef48c5b0
Ir1d <sirius.caffrey@gmail.com>
2019-06-25 19:26:09 +0800

    upd

--- src/saber-browser.js
+++ src/saber-browser.js
@@ -39,8 +39,8 @@ export default ({ router, setHead, rootOptions }) => {
         {
           type: "text/javascript",
           src:
-            "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML",
-          async: true
+            "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML"
+          // async: true
         }
       ]
     };
